e04b4f883c20df351576ac8653b1e7f19af4c1d2
Jonathan.Oliver <Jonathan.Oliver@CA0139>
2019-06-21 07:55:42 +0100

    Add new version of Missiles

---
+++ MissilesPyGame/Missiles.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Interfaces;
+using MissilesPyGame.Models;
+
+namespace MissilesPyGame
+{
+    public class Missiles : PyGame
+    {
+        private const int WIDTH = 800;
+        private const int HEIGHT = 400;
+
+        private readonly IRandom _random;
+
+        private List<Missile> _missiles;
+
+        public Missiles(ITimerFactory timerFactory, IRandom random) 
+            : base(WIDTH, HEIGHT, timerFactory)
+        {
+            _random = random;
+
+            _missiles = new List<Missile>();
+            NewMissile();
+
+            ScheduleClockInterval(NewMissile, 5000);
+        }
+
+        public override void Draw(Graphics g)
+        {
+            foreach (var missile in _missiles)
+            {
+                missile.Draw(g);
+            }
+        }
+
+        public override void Update(TimeSpan timeSinceLastUpdate, Graphics g)
+        {
+            for (int i = _missiles.Count - 1; i > -1; i--)
+            {
+                var missile = _missiles[i];
+                missile.Update(timeSinceLastUpdate);
+                if (missile.IsTrailOffBottomOfScreen())
+                {
+                    _missiles.Remove(missile);
+                }
+            }
+        }
+
+        public void NewMissile()
+        {
+            var missile = new Missile(_random.Next(400, WIDTH),
+                _random.Next(-70, -10),
+                TimeSpan.FromSeconds(_random.Next(0, 3)),
+                HEIGHT);
+
+            _missiles.Add(missile);
+        }
+    }
+}
